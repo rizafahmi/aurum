81d725fffa5b289e92b93654e3e69af68d6e0ea1
Riza Fahmi <rizafahmi@gmail.com>
2026-03-29 08:25:58 +0700

    test: fix page controller test to use absolute path
    - Change from ~p"/" sigil to absolute "/" path
    - Maintain existing test assertion for home page content

--- test/aurum_web/controllers/page_controller_test.exs
+++ test/aurum_web/controllers/page_controller_test.exs
@@ -2,7 +2,8 @@ defmodule AurumWeb.PageControllerTest do
   use AurumWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
+    conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
   end
 end
+
